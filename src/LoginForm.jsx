c870e9295b6f8b502af4a767a42c9fe0821fb9e0
Mansour Mbengue <mbgmansour13@gmail.com>
2026-05-21 16:25:16 +0200

    feat: add LoginForm component with input and submit button

---
+++ src/LoginForm.jsx
@@ -0,0 +1,10 @@
+export default function LoginForm() {
+  return (
+    <>
+      <form action="submit">
+        <input type="text" placeholder="Enter your first name" />
+        <button type="submit">Access your space</button>
+      </form>
+    </>
+  );
+}
