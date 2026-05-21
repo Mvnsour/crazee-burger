245347be1e722c1b83242982a64bb516928dec2f
Mansour Mbengue <mbgmansour13@gmail.com>
2026-05-21 19:48:04 +0200

    feat: add LoginForm component to Login page

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,8 +1,11 @@
+import LoginForm from "./LoginForm";
+
 export default function Login() {
   return (
     <>
       <h1>Welcome to our site</h1>
       <h3>Sign in</h3>
+      <LoginForm />
     </>
   );
 }
