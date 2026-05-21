c0752ad066391c8b340b6627b88f517d008a82c5
Mansour Mbengue <mbgmansour13@gmail.com>
2026-05-21 19:47:59 +0200

    feat: implement LoginForm with state management for first name input

--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -1,8 +1,28 @@
+import { useState } from "react";
+
 export default function LoginForm() {
+  const [firstName, setFirstName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Welcome to your space ${firstName}`);
+    setFirstName("");
+  };
+
+  const handleChange = (e) => {
+    setFirstName(e.target.value);
+  };
+
   return (
     <>
-      <form action="submit">
-        <input type="text" placeholder="Enter your first name" />
+      <form action="submit" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={firstName}
+          placeholder="Enter your first name"
+          onChange={handleChange}
+          required
+        />
         <button type="submit">Access your space</button>
       </form>
     </>
