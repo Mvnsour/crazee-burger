cbf7004dd0adefe6924ef0302a62c403e82804db
Mansour Mbengue <mbgmansour13@gmail.com>
2026-07-02 07:34:12 +0200

    ui F03: LOGO > update Logo component and style added

--- src/components/reusable-ui/Logo.jsx
+++ src/components/reusable-ui/Logo.jsx
@@ -4,13 +4,35 @@ import styled from "styled-components";
 export default function Logo() {
   return (
     <LogoStyled>
-      <span>Crazee</span>
-      <img src={burgerLogo} alt="Crazee Burger" />
-      <span>Burger</span>
+      <h1>Crazee</h1>
+      <img src={burgerLogo} alt="Logo Crazee Burger" />
+      <h1>Burger</h1>
     </LogoStyled>
   );
 }
 
 const LogoStyled = styled.div`
-  background: blue;
+  display: flex;
+  align-items: center;
+  transform: scale(2.5);
+
+  h1 {
+    display: inline;
+    color: #ffa01b;
+    text-align: center;
+    text-transform: uppercase;
+    text-spacing: 1.5px;
+    font-size: 36px;
+    line-height: 1rem;
+    font-weight: 700;
+    font-family: "Amatic SC", cursive;
+  }
+
+  img {
+    width: 80px;
+    height: 60px;
+    margin: 0 5px;
+    object-fit: contain;
+    object-position: center;
+  }
 `;
